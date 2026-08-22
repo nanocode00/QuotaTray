606e5c2f3650313a2c120d6f368bcaf02b7215ef
JaeHun Kim <rwg0901@naver.com>
2026-08-22 15:49:27 +0900

    test: add debug Codex Spark mock launch flag

--- QuotaTray.Desktop/Program.cs
+++ QuotaTray.Desktop/Program.cs
@@ -1,13 +1,23 @@
 using System;
+using System.Linq;
 using Avalonia;
 
 namespace QuotaTray.Desktop;
 
 internal class Program
 {
+    public static bool UseCodexSparkMock { get; private set; }
+
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args, Avalonia.Controls.ShutdownMode.OnExplicitShutdown);
+    public static void Main(string[] args)
+    {
+#if DEBUG
+        UseCodexSparkMock = args.Any(a => a.Equals("--mock-codex-spark", StringComparison.OrdinalIgnoreCase));
+#endif
+
+        BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args, Avalonia.Controls.ShutdownMode.OnExplicitShutdown);
+    }
 
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
